54ac85bb73470734b5b015bb3a4f82d4531d1c14
zebinfzu <1185261506@qq.com>
2023-07-27 11:22:52 +0800

    nvim: add some keymaps

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("core/basic")
+require("core/keymaps")
 require("plugins")
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,14 @@
+-- leader key 为空格
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = {
+  noremap = true,
+  silent = true,
+}
+
+-- 本地变量
+local map = vim.api.nvim_set_keymap
+
+-- jj回到normal模式
+map("i", "jj", "<ESC>", opt)
