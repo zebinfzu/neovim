9be5bc5669b578cc5f5e6b625590e05b3e2ccd55
zebinfzu <1185261506@qq.com>
2023-07-27 17:49:21 +0800

    update

--- lua/plugins/config/colorscheme.lua
+++ lua/plugins/config/colorscheme.lua
@@ -1,49 +1,56 @@
 -- vim.cmd.colorscheme("base16-google-dark")
-require('onedark').setup  {
-    -- Main options --
-    style = 'warmer', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = true,  -- Show/hide background
-    term_colors = true, -- Change terminal color as per the selected theme style
-    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+require('onedark').setup {
+  -- Main options --
+  style = 'warmer',             -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+  transparent = false,          -- Show/hide background
+  term_colors = true,           -- Change terminal color as per the selected theme style
+  ending_tildes = false,        -- Show the end-of-buffer tildes. By default they are hidden
+  cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
-    -- toggle theme style ---
-    toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+  -- toggle theme style ---
+  toggle_style_key = nil,                                                              -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+  toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
 
-    -- Change code style ---
-    -- Options are italic, bold, underline, none
-    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-    code_style = {
-        comments = 'italic',
-        keywords = 'none',
-        functions = 'bold',
-        strings = 'none',
-        variables = 'none'
-    },
+  -- Change code style ---
+  -- Options are italic, bold, underline, none
+  -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+  code_style = {
+    comments = 'italic',
+    keywords = 'none',
+    functions = 'bold',
+    strings = 'none',
+    variables = 'bold'
+  },
 
-    -- Lualine options --
-    lualine = {
-        transparent = true, -- lualine center bar transparency
-    },
+  -- Lualine options --
+  lualine = {
+    transparent = true, -- lualine center bar transparency
+  },
 
-    -- Custom Highlights --
-    colors = {
-      bright_orange = "#ff8800",
-      sliver = "#D3D3D3",
-    }, -- Override default colors
-    highlights = {
-      -- 当前行号颜色
-      CursorLineNr = {fg = '$bright_orange'},
-      -- 行号颜色
-      LineNr = { fg = '$sliver'},
-    }, -- Override highlight groups
+  -- Custom Highlights --
+  colors = {
+    bright_orange = "#ff8800",
+    sliver = "#D3D3D3",
+    navajo_white = "#FFDEAD",
+    light_steel_blue = "#B0C4DE",
+    gold = "#FFD700",
+  }, -- Override default colors
+  highlights = {
+    -- 当前行号颜色
+    CursorLineNr = { fg = '$bright_orange' },
+    -- 当前行
+    -- CursorLine = { bg = '$light_steel_blue' },
+    -- 行号颜色
+    LineNr = { fg = '$sliver' },
+    Comment = { fg = '$navajo_white' },
+    Variables = { bg = '$sliver', fg = '$gold' },
+  }, -- Override highlight groups
 
-    -- Plugins Config --
-    diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true,   -- use undercurl instead of underline for diagnostics
-        background = true,    -- use background color for virtual text
-    },
+  -- Plugins Config --
+  diagnostics = {
+    darker = true,     -- darker colors for diagnostic
+    undercurl = true,  -- use undercurl instead of underline for diagnostics
+    background = true, -- use background color for virtual text
+  },
 }
 require('onedark').load()
