bb4438bcbbb5bd4f4bb6a567170427f896558c1c
zebinfzu <1185261506@qq.com>
2023-07-27 18:04:11 +0800

    update

--- lua/plugins/config/rust-config.lua
+++ lua/plugins/config/rust-config.lua
@@ -32,10 +32,10 @@ rt.setup({
       show_parameter_hints = true,
       -- prefix for parameter hints
       -- default: "<-"
-      parameter_hints_prefix = "<-",
+      parameter_hints_prefix = "<- ",
       -- prefix for all the other hints (type, chaining)
       -- default: "=>"
-      other_hints_prefix = "=>",
+      other_hints_prefix = "=> ",
       -- whether to align to the length of the longest line in the file
       max_len_align = false,
       -- padding from the left if max_len_align is true
